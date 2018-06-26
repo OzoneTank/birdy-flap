e19a878c90d0d90e910367c480b1b6cd8c1844d1
Ed Cole (OzoneTank) <ed.cole@smashingboxes.com>
2018-06-25 23:17:28 -0400

    birdy flap game

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public float flapStrength = 10.0f;
+    Rigidbody2D rb;
+
+    public GameObject game;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+        GameScript gameScript = game.GetComponent<GameScript> ();
+
+        if (gameScript.gameMode == GameScript.GameMode.running) {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        } else {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.velocity = Vector2.zero;
+        }
+        if (Input.GetMouseButtonDown (0) ||
+        Input.GetKeyDown (KeyCode.Space)) {
+            Flap ();
+        }
+	}
+
+    void Flap() {
+        GameScript gameScript = game.GetComponent<GameScript> ();
+        if (gameScript.gameMode == GameScript.GameMode.waiting) {
+            gameScript.StartGame ();
+            rb.velocity = Vector2.zero;
+        }
+
+        if (gameScript.gameMode == GameScript.GameMode.running) {
+            Vector2 flapForce = new Vector2 (0, flapStrength);
+            rb.AddForce (flapForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        game.GetComponent<GameScript> ().GameOver ();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Pipe") {
+            game.GetComponent<GameScript> ().Score ();
+        }
+    }
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour {
+
+    public GameObject bird;
+
+    public GameObject pipesPrefab;
+    public float moveSpeed = 1.0f;
+    public float defaultMoveSpeed = 1.0f;
+    public float moveSpeedInc = 0.02f;
+    public float spawnRate = 3.0f;
+
+    public float width = 10.0f;
+    public float min = -5.0f;
+    public float max = 5.0f;
+
+    public float spawnRight = 10.0f;
+    public float despawnLeft = -10.0f;
+
+    private List<GameObject> pipesList = new List<GameObject> ();
+    private float lastTimeSpawn;
+
+    private int score;
+
+    public Text scoreUI;
+
+
+    public enum GameMode {
+        waiting,
+        running,
+        gameOver
+    };
+
+    public GameMode gameMode = GameMode.running;
+
+	void Start () {
+        Restart ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameMode == GameMode.running) {
+            if (Time.time > lastTimeSpawn + spawnRate) {
+                lastTimeSpawn += spawnRate;
+                SpawnPipe ();
+            }
+        }
+
+	}
+
+    void FixedUpdate() {
+        if (gameMode == GameMode.running) {
+            List <GameObject> pipesToDestroy = new List<GameObject> ();
+            foreach (GameObject pipes in pipesList) {
+                Vector2 pos = pipes.transform.position;
+                pipes.transform.position = pos + (Vector2.left * moveSpeed * Time.deltaTime);
+
+                if (pos.x < despawnLeft) {
+                    pipesToDestroy.Add (pipes);
+                }
+            }
+            foreach (GameObject pipes in pipesToDestroy) {
+                Destroy (pipes);
+                pipesList.Remove (pipes);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player") {
+            GameOver ();
+        }
+    }
+
+    public void GameOver() {
+        gameMode = GameMode.gameOver;
+    }
+
+    public void Score() {
+        score++;
+        scoreUI.text = "" + score;
+        moveSpeed += moveSpeedInc;
+    }
+
+    public void StartGame() {
+        gameMode = GameMode.running;
+        lastTimeSpawn = Time.time;
+    }
+
+    public void Restart() {
+        foreach(GameObject pipes in pipesList)
+        {
+            Destroy(pipes);
+        }
+        pipesList.Clear ();
+        gameMode = GameMode.waiting;
+        bird.transform.position = new Vector2 (0, 0);
+        score = 0;
+        scoreUI.text = "0";
+        moveSpeed = defaultMoveSpeed;
+    }
+
+    void SpawnPipe() {
+        Vector2 pos = new Vector3 (spawnRight, Random.Range (min, max), 100.0f);
+        GameObject pipes = Instantiate (pipesPrefab, pos, transform.rotation) as GameObject;
+        pipesList.Add (pipes);
+    }
+}
